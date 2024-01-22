48bafe711251391a7d675fbfaee8224344702ab0
Kelly Schumacher <kschumacher93@gmail.com>
2024-01-22 13:20:17 -0500

    added movies to the DOM

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a3e520c290a5499588b6c63ab4210271&page=1';
-const IMG_PATH = 'https://image.thttps://image.tmdb.org/t/p/w1280';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=a3e520c290a5499588b6c63ab4210271&query="';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-const main = document.getElementById('main');
 
 getMovies(API_URL);
 
@@ -12,8 +12,40 @@ async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
     
-    console.log(data.results);
+    function showMovies(movies) {
+        main.innerHTML = '';
+        movies.forEach(movie => {
+            const { title, poster_path, vote_average, overview } = movie;
+            const movieEl = document.createElement('div');
+            movieEl.classList.add('movie');
+            movieEl.innerHTML = `
+                <img src="${IMG_PATH + poster_path}" alt="${title}">
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(vote_average)}">${vote_average.toFixed(1)}</span>
+                    </div>
+                    <div class="overview">
+                    <h3>Overview</h3>
+                    ${overview ? overview : 'No Overview'}
+                </div>
+                
+            `
+            main.appendChild(movieEl)
+        });
+
     }
+    showMovies(data.results);
+}
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green';
+    } else if(vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
